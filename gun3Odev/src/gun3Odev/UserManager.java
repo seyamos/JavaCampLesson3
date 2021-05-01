37f124468b170df80950d9b074eb67e6710fb924
seydaerbay <seydaerbay@outlook.com>
2021-05-02 02:32:35 +0300

    update

--- gun3Odev/src/gun3Odev/UserManager.java
+++ gun3Odev/src/gun3Odev/UserManager.java
@@ -2,11 +2,11 @@ package gun3Odev;
 
 public class UserManager {
 	public void add(User user) {
-		System.out.println("Kullanýcý eklendi: " + user.getEmail());
+		System.out.println("Kullanýcý eklendi:  " + user.getEmail());
 	}
 	
 	public void remove(User user) {
-		System.out.println("Kullanýcý silindi: " + user.getEmail());
+		System.out.println("Kullanýcý silindi:  " + user.getEmail());
 	}	
 	
 	
