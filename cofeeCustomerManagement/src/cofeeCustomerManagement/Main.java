dc52afd56ec708efdd5b83f496a5331591676ea9
seydaerbay <seydaerbay@outlook.com>
2021-05-04 19:47:44 +0300

    name updated

--- cofeeCustomerManagement/src/cofeeCustomerManagement/Main.java
+++ cofeeCustomerManagement/src/cofeeCustomerManagement/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		BaseCustomerManager starbucksCustomerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
-		starbucksCustomerManager.save(new Customer(1,"Þeyda","Erbay",new Date(1976,1,1), "22222222222"));
+		starbucksCustomerManager.save(new Customer(1,"Þeyda","Erbay",new Date(1976,1,1), "2222222222"));
 		
 		BaseCustomerManager neroCustomerManager = new NeroCustomerManager();
 		neroCustomerManager.save(new Customer(1,"Þeyda","Erbay",new Date(1976,1,1), "22222222222"));
